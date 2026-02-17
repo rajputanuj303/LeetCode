fe133c5b779da6bfa383db55a3fbeffa338742e8
rajputanuj303 <143288137+rajputanuj303@users.noreply.github.com>
2026-02-17 19:53:42 +0530

    Time: 85 ms (11.79%) | Memory: 38.2 MB (5.01%) - LeetSync

--- 264-ugly-number-ii/ugly-number-ii.cpp
+++ 264-ugly-number-ii/ugly-number-ii.cpp
@@ -1,25 +1,42 @@
 class Solution {
 public:
     int nthUglyNumber(int n) {
-        vector<long long> dp(n+1);
-        dp[0] = 1;
-        long long u2 = 0;
-        long long u3 = 0;
-        long long u5 = 0;
 
-        for(int i = 1;i<n;i++){
-            long long newU2 = dp[u2] * 2;
-            long long newU3 = dp[u3] * 3;
-            long long newU5 = dp[u5] * 5;
+        if(n == 1) return 1;
 
-            long long newAns = min({newU2,newU3,newU5});
-            if(newAns == newU2) u2++;
-            if(newAns == newU3) u3++;
-            if(newAns == newU5) u5++;
 
-            dp[i] = newAns;
+        n--;
+
+        priority_queue<long long, vector<long long>, greater<long long>> pq;
+        unordered_map<long long, bool> visited;
+
+        pq.push(2);
+        pq.push(3);
+        pq.push(5);
+
+        while(--n){
+
+            long long curr = pq.top(); pq.pop();            
+
+            long long m1 = curr*2;
+            long long m2 = curr*3;
+            long long m3 = curr*5;
+
+            if(visited.find(m1) == visited.end()){
+                visited[m1] = true;
+                pq.push(m1);
+            }
+            if(visited.find(m2) == visited.end()){
+                visited[m2] = true;
+                pq.push(m2);
+            }
+
+            if(visited.find(m3) == visited.end()){
+                visited[m3] = true;
+                pq.push(m3);
+            }
+
         }
-
-        return dp[n-1];
+        return pq.top();
     }
 };
