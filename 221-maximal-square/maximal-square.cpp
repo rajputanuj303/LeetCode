c13877fe5fefd3f0b8f4455f2b79cd074f2b15bd
rajputanuj303 <143288137+rajputanuj303@users.noreply.github.com>
2026-02-19 12:23:45 +0530

    Time: 7 ms (73.85%) | Memory: 31 MB (63.57%) - LeetSync

--- 221-maximal-square/maximal-square.cpp
+++ 221-maximal-square/maximal-square.cpp
@@ -28,10 +28,6 @@ public:
             }
         }
 
-        for(int i = 0; i<m; i++){
-            for(int j = 0; j<n; j++) cout << dp[i][j] << " ";
-            cout << endl;
-        }
         return maxSize*maxSize;
     }
 };
