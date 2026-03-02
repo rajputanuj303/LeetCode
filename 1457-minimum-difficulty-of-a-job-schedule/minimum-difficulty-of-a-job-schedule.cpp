b20b45fa4d274c13c4a17e2df2413fed89eee674
rajputanuj303 <143288137+rajputanuj303@users.noreply.github.com>
2026-03-02 23:28:12 +0530

    Time: 49 ms (34.77%) | Memory: 11.2 MB (52.66%) - LeetSync

--- 1457-minimum-difficulty-of-a-job-schedule/minimum-difficulty-of-a-job-schedule.cpp
+++ 1457-minimum-difficulty-of-a-job-schedule/minimum-difficulty-of-a-job-schedule.cpp
@@ -1,28 +1,25 @@
 class Solution {
 public:
 
-    int Solver(int l, int r, int d, vector<int> &arr, vector<vector<vector<int>>> &dp){
+    int Solver(int idx, int d, int &n, vector<int> &arr, vector<vector<int>> &dp){
 
-        if(r-l+1 < d) return 1e9+7;
         if(d == 1){
-
             int maxD = 0;
-            for(int i = l; i<=r; i++) maxD = max(maxD, arr[i]);
-
+            for(int i = idx; i<n; i++) maxD = max(maxD, arr[i]);
             return maxD;
         }
 
-        if(dp[l][r][d] != -1) return dp[l][r][d];
+        if(dp[idx][d] != -1) return dp[idx][d];
 
         int mini = INT_MAX;
         int maxD = 0;
 
-        for(int mid = l; mid<r; mid++){
-            maxD = max(maxD, arr[mid]);
-            int val = maxD + Solver(mid+1, r, d-1, arr, dp);
+        for(int i = idx; i <= n-d; i++){
+            maxD = max(maxD, arr[i]);
+            int val = maxD + Solver(i+1, d-1, n, arr, dp);
             mini = min(mini, val);
         }
-        return dp[l][r][d] = mini;
+        return dp[idx][d] = mini;
     }
 
     int minDifficulty(vector<int>& jobDifficulty, int d) {
@@ -30,8 +27,8 @@ public:
 
         if(n < d) return -1;
 
-        vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(n+1, vector<int>(d+1, -1)));
+        vector<vector<int>> dp(n+1, vector<int>(d+1, -1));
 
-        return Solver(0, n-1, d, jobDifficulty, dp);
+        return Solver(0, d, n, jobDifficulty, dp);
     }
 };
