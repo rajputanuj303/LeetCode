e68c706071c07f44f22f12ba06e834fadf3d578d
rajputanuj303 <143288137+rajputanuj303@users.noreply.github.com>
2026-03-06 22:28:45 +0530

    Time: 114 ms (17.70%) | Memory: 50.1 MB (17.12%) - LeetSync

---
+++ 923-super-egg-drop/super-egg-drop.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<vector<int>> dp;
+
+    int Solver(int n, int e){
+        if(e == 1 || n <= 1) return n;
+
+        if(dp[n][e] != -1) return dp[n][e];
+
+        int low = 1, high = n;
+        int ans = INT_MAX;
+
+        while(low <= high){
+            int mid = (low + high) / 2;
+
+            int broken = (dp[mid-1][e-1] != -1) ? dp[mid-1][e-1] : Solver(mid-1, e-1);
+            int notbroken = (dp[n-mid][e] != -1) ? dp[n-mid][e] : Solver(n-mid, e);
+
+            int temp = 1 + max(broken, notbroken);
+
+            if(broken > notbroken){
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
+            }
+
+            ans = min(ans, temp);
+        }
+
+        return dp[n][e] = ans;
+    }
+
+    int superEggDrop(int k, int n) {
+        dp.resize(n+1, vector<int>(k+1, -1));
+        return Solver(n, k);
+    }
+};
