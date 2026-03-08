0cbade96e83f1efaba27ff6fd439fd26cf427588
rajputanuj303 <143288137+rajputanuj303@users.noreply.github.com>
2026-03-08 10:18:35 +0530

    Time: 2535 ms (5.08%) | Memory: 428.1 MB (5.08%) - LeetSync

---
+++ 2107-find-unique-binary-string/find-unique-binary-string.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+
+    string toBinary(int n, int size){
+
+        string s = "";     
+        while(n > 0){
+            s = char('0' + (n % 2)) + s;
+            n /= 2;
+        }
+
+        while(s.size() < size) s = '0' + s;
+
+        return s;
+    }
+
+    void generateBits(int n, vector<string> &bits){
+
+        for(int i = 0; i<pow(2, n); i++){
+            string s = toBinary(i, n);
+            bits.push_back(s);
+        }
+    }
+
+    string findDifferentBinaryString(vector<string>& nums) {
+        set<string> stt(nums.begin(), nums.end());
+
+        int n = nums[0].size();
+        vector<string> bits;
+
+        generateBits(n, bits);
+
+        for(string s : bits){
+            if(stt.find(s) == stt.end()) return s;
+        }
+        return "";
+    }
+};
