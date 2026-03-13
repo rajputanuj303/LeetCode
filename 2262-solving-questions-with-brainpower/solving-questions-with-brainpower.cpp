bdeb405f9c2c4307da877b7b7358e42783944174
rajputanuj303 <143288137+rajputanuj303@users.noreply.github.com>
2026-03-14 00:11:28 +0530

    Time: 0 ms (100.00%) | Memory: 119.8 MB (79.74%) - LeetSync

--- 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.cpp
+++ 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
-    int n;
-    vector<long long> dp;
-
-    long long Solver(int i, vector<vector<int>> &questions){
-        if(i >= n) return 0;
-
-        if(dp[i] != -1) return dp[i];
-
-        long long notpick = Solver(i+1, questions);
-        long long pick = questions[i][0] + Solver(i+questions[i][1]+1, questions);
-
-        return dp[i] = max(pick, notpick);
-    }
-
-
     long long mostPoints(vector<vector<int>>& questions) {
-        n = questions.size();
-        dp.resize(n+1, -1);
-        return Solver(0, questions);
+        
+        int n = questions.size();
+
+        vector<long long> dp(n+1, 0);
+
+
+        for(int i = n-1; i>= 0; i--){
+
+            long long notpick = dp[i+1];
+            long long pick = questions[i][0];            
+            if(i+questions[i][1]+1 <= n) pick += dp[i+questions[i][1]+1];
+            
+            dp[i] = max(pick, notpick);
+
+        }
+
+        return dp[0];
     }
 };
