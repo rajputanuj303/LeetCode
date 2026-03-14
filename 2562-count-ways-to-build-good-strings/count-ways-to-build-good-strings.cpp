c692c282cf51566b85adb2652b96f90567d61bd1
rajputanuj303 <143288137+rajputanuj303@users.noreply.github.com>
2026-03-14 19:49:28 +0530

    Time: 12 ms (9.87%) | Memory: 20.9 MB (31.99%) - LeetSync

--- 2562-count-ways-to-build-good-strings/count-ways-to-build-good-strings.cpp
+++ 2562-count-ways-to-build-good-strings/count-ways-to-build-good-strings.cpp
@@ -8,19 +8,13 @@ public:
 
         if(dp[s] != -1) return dp[s];
 
-        int count = 0;
+        long long count = 0;
 
-        if(s+z >= l && s+z <= h){
-            count = 1 + (Solver(s+z, l, h, z, o)%MOD);
-        }else{
-            count = Solver(s+z, l, h, z, o)%MOD;
-        }
+        if(s >= l && s <= h) 
+            count = 1;
 
-        if(s+o >= l && s+o <= h){
-            count += (1 + Solver(s+o, l, h, z, o))%MOD;
-        }else{
-            count = (count%MOD + Solver(s+o, l, h, z, o)%MOD)%MOD;
-        }
+        count = (count + Solver(s + z, l, h, z, o)) % MOD;
+        count = (count + Solver(s + o, l, h, z, o)) % MOD;
 
         return dp[s] = count%MOD;
     }
