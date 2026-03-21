3c35d47a4d2cfff4ba6441b551de1f71cd12c16e
rajputanuj303 <143288137+rajputanuj303@users.noreply.github.com>
2026-03-21 22:15:34 +0530

    Time: 819 ms (8.45%) | Memory: 129.4 MB (6.17%) - LeetSync

---
+++ 1290-make-array-strictly-increasing/make-array-strictly-increasing.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    unordered_map<string, int> dp;
+
+    int Solver(int i, int prev, vector<int>& arr1, vector<int>& arr2){
+
+        if(i >= arr1.size()) return 0;
+
+        string key = to_string(i) + "#" + to_string(prev);
+        if(dp.count(key)) return dp[key];
+
+        int nottake = 1e9;
+        int take = 1e9;
+
+        // Option 1: don't replace
+        if(arr1[i] > prev){
+            nottake = Solver(i+1, arr1[i], arr1, arr2);
+        }
+
+        // Option 2: replace using upper_bound
+        auto it = upper_bound(arr2.begin(), arr2.end(), prev);
+
+        if(it != arr2.end()){
+            take = 1 + Solver(i+1, *it, arr1, arr2);
+        }
+
+        return dp[key] = min(take, nottake);
+    }
+
+    int makeArrayIncreasing(vector<int>& arr1, vector<int>& arr2) {
+
+        sort(arr2.begin(), arr2.end());
+
+        int res = Solver(0, -1, arr1, arr2);
+
+        return (res >= 1e9) ? -1 : res;
+    }
+};
