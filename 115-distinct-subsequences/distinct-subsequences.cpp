ecc3a4e83a2b87de1ab4e0e9f39c2f3fb91adda9
rajputanuj303 <143288137+rajputanuj303@users.noreply.github.com>
2026-04-08 00:36:42 +0530

    Time: 1427 ms (7.87%) | Memory: 411.5 MB (7.69%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,29 +1,24 @@
 class Solution {
 public:
     int m, n;
-    vector<vector<long long>> dp;
-
-    long long Solver(int i, int j, string &s, string &t){
+    vector<vector<int>> dp;
+    int Solver(int i, int j, string s, string t){
         if(j == n) return 1;
         if(i == m) return 0;
 
         if(dp[i][j] != -1) return dp[i][j];
 
-        long long count = 0;
+        int count = 0;
 
-        if(s[i] == t[j]){
-            count += Solver(i+1, j+1, s, t); // take
-        }
-
-        count += Solver(i+1, j, s, t); // skip
+        if(s[i] == t[j]) count += (dp[i+1][j+1] != -1) ? dp[i+1][j+1] : Solver(i+1, j+1, s, t); // take
+        count += (dp[i+1][j] != -1) ? dp[i+1][j] : Solver(i+1, j, s, t); // skip        
 
         return dp[i][j] = count;
     }
-
     int numDistinct(string s, string t) {
         m = s.size();
         n = t.size();
-        dp.resize(m, vector<long long>(n, -1));
+        dp.resize(m+1, vector<int>(n+1, -1));
         return Solver(0, 0, s, t);
     }
 };
