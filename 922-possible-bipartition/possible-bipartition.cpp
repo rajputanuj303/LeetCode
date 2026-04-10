ca2fe73790b3b061efadb5bd33d74f60482bf575
rajputanuj303 <143288137+rajputanuj303@users.noreply.github.com>
2026-04-10 23:34:14 +0530

    Time: 73 ms (8.32%) | Memory: 78.6 MB (7.56%) - LeetSync

---
+++ 922-possible-bipartition/possible-bipartition.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+
+    //-1 --> N/A, 0 --> R, 1 --> G
+
+    bool dfs(int i, int col, vector<int> &visited, unordered_map<int, vector<int>> &adjLs){
+        
+        visited[i] = col;
+
+        for(int ele : adjLs[i]){
+            if(visited[ele] == -1){
+                if(!dfs(ele, 1 - col, visited, adjLs)) return false;
+            }else if(visited[ele] == col) return false;
+        }
+
+        return true;
+    }
+
+    bool possibleBipartition(int n, vector<vector<int>>& dislikes) {
+        unordered_map<int, vector<int>> adjLs;
+
+        for(vector<int> v : dislikes){
+            adjLs[v[0]].push_back(v[1]);
+            adjLs[v[1]].push_back(v[0]);
+        }
+
+        vector<int> visited(n+1, -1);
+
+        for(int i = 1; i <= n; i++){
+            if(visited[i] == -1){
+                if(!dfs(i, 0, visited, adjLs)) return false;
+            }
+        }
+
+        return true;
+    }
+};
