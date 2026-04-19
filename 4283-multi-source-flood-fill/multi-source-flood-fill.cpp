d50d431c8a96f801b05aaafdc6ae6787443ae149
rajputanuj303 <143288137+rajputanuj303@users.noreply.github.com>
2026-04-19 22:16:15 +0530

    Time: 135 ms (82.97%) | Memory: 194 MB (98.19%) - LeetSync

--- 4283-multi-source-flood-fill/multi-source-flood-fill.cpp
+++ 4283-multi-source-flood-fill/multi-source-flood-fill.cpp
@@ -1,34 +1,34 @@
 class Solution {
 public:
+    static bool cmp(vector<int> &u, vector<int> &v){
+        return u[2] > v[2];
+    }
     vector<vector<int>> colorGrid(int n, int m, vector<vector<int>>& sources) {
         
 
         vector<vector<int>> res(n, vector<int>(m, 0));
-        vector<vector<int>> time(n, vector<int>(m, -1));
 
         queue<pair<int, int>> que;
+
+        sort(sources.begin(), sources.end(), cmp);
 
         for(vector<int> &v : sources){
             
             int i = v[0], j = v[1], color = v[2];
-
             res[i][j] = color;
-            time[i][j] = 0;
-
             que.push({i, j});
         }
 
         vector<pair<int, int>> dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 
         while(!que.empty()){
-            int s = que.size();
 
+            int s = que.size();
             while(s--){
                 int i = que.front().first;
                 int j = que.front().second;
                 que.pop();
 
-                int currtime = time[i][j];
                 int currColor = res[i][j];
 
                 for(pair<int, int> &dir : dirs){
@@ -37,15 +37,10 @@ public:
 
                     if(newi >= 0 && newj >= 0 && newi < n && newj < m){
 
-                        if(time[newi][newj] == -1){
-
-                            time[newi][newj] = currtime+1;
+                        if(res[newi][newj] == 0){
+                            
                             res[newi][newj] = currColor;
                             que.push({newi, newj});
-
-                        }
-                        else if(time[newi][newj] == currtime+1){
-                            res[newi][newj] = max(res[newi][newj], currColor);
                         }
                     }
                 }
