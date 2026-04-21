80deff425a718f2714683c36d6bd80764bee017c
rajputanuj303 <143288137+rajputanuj303@users.noreply.github.com>
2026-04-21 15:48:06 +0530

    Time: 274 ms (4.88%) | Memory: 12.2 MB (5.69%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/split-array-largest-sum.cpp
@@ -2,13 +2,12 @@ class Solution {
 public:
     int n;
     vector<vector<int>> dp;
+    vector<int> prefixSum;
     int Solver(int idx, int k, vector<int> &nums){
         if(k > nums.size()) return INT_MAX;
         if(k == 1){
-            int sum = 0;
-            for(int i = idx; i<nums.size(); i++){
-                sum += nums[i];
-            }
+            int sum = prefixSum[n-1];
+            if(idx-1 >= 0) sum -= prefixSum[idx-1];
             return sum;
         }
 
@@ -25,8 +24,15 @@ public:
         return dp[idx][k] = mini;
     }
     int splitArray(vector<int>& nums, int k) {
-        int n = nums.size();
+        n = nums.size();
         dp.resize(n+1, vector<int>(k+1, -1));
+
+        prefixSum.resize(n, 0);
+        prefixSum[0] = nums[0];
+
+        for(int i = 1; i<n; i++){
+            prefixSum[i] = prefixSum[i-1] + nums[i];
+        }
         return Solver(0, k, nums);
     }
 };
