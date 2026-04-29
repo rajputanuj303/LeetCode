1ec6b0a0d299be56e0e38ec5e77883faa4c401da
rajputanuj303 <143288137+rajputanuj303@users.noreply.github.com>
2026-04-29 12:55:39 +0530

    Time: 85 ms (49.35%) | Memory: 182.2 MB (27.95%) - LeetSync

--- 146-lru-cache/lru-cache.cpp
+++ 146-lru-cache/lru-cache.cpp
@@ -1,7 +1,6 @@
 class LRUCache {
 public:
-    list<int> dll; // front is latest and back is oldest
-
+    list<int> dll; // front is MRU and back is LRU
     // {key, {value, node-iterator}}
     unordered_map<int, pair<int, list<int>::iterator>> cache;
 
@@ -10,23 +9,23 @@ public:
     LRUCache(int capacity) {
         cap = capacity;
     }
-    
-    int get(int key) {
-        if(!cache.count(key)) return -1;
 
+    void UpdateKey(int key){
         dll.erase(cache[key].second);
         dll.push_front(key);
         cache[key].second = dll.begin();
-        
+    }
+    
+    int get(int key) {
+        if(!cache.count(key)) return -1;
+        UpdateKey(key);
         return cache[key].first;
     }
     
     void put(int key, int value) {
         if(cache.count(key)){
             cache[key].first = value;
-            dll.erase(cache[key].second);
-            dll.push_front(key);
-            cache[key].second = dll.begin();
+            UpdateKey(key);
         }else{
             dll.push_front(key);
             cache[key] = {value, dll.begin()};
@@ -36,9 +35,7 @@ public:
         if(cap < 0){
             int keyDel = dll.back();
             cache.erase(keyDel);
-
             dll.pop_back();
-
             cap++;
         }
     }
