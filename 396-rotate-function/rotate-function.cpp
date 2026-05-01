4bd35b1724df232e92fe96ff75252db822bd26de
rajputanuj303 <143288137+rajputanuj303@users.noreply.github.com>
2026-05-02 00:54:53 +0530

    Time: 0 ms (100.00%) | Memory: 99.7 MB (21.33%) - LeetSync

--- 396-rotate-function/rotate-function.cpp
+++ 396-rotate-function/rotate-function.cpp
@@ -4,19 +4,18 @@ public:
 
         int n = nums.size();       
 
-        int prevF = 0;
+        int Fi = 0;
         int sum = 0;
         for(int i = 0; i<n; i++){
-            prevF += (i*nums[i]);
+            Fi += (i*nums[i]);
             sum += nums[i];
         }
 
-        int maxi = prevF;
+        int maxi = Fi;
 
         for(int i = 1; i<n; i++){
-            int currF = prevF + (sum - n*nums[n-i]);
-            maxi = max(maxi, currF);
-            prevF = currF;
+            Fi = Fi + (sum - n*nums[n-i]);
+            maxi = max(maxi, Fi);
         }
 
         return maxi;
