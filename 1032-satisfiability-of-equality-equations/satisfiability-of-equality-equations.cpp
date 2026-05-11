974d6a113cd6044758f91035dc0288c293401a91
Anuj Rajput <143288137+rajputanuj303@users.noreply.github.com>
2026-05-12 01:01:35 +0530

    Time: 0 ms (100.00%) | Memory: 14.7 MB (99.34%) - LeetSync

---
+++ 1032-satisfiability-of-equality-equations/satisfiability-of-equality-equations.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    vector<char> parents;
+
+    char find(char x){
+
+        if(parents[int(x-'a')] == x) return x;
+
+        return find(parents[int(x-'a')]);
+    }
+
+    void unite(char a, char b){
+
+        char pa = find(a);
+        char pb = find(b);
+
+        if(pa == pb) return;
+
+        parents[int(pb-'a')] = pa;
+    }
+    
+    bool equationsPossible(vector<string>& equations) {
+
+        for(char c = 'a'; c<='z'; c++){
+            parents.push_back(c);
+        }
+
+        for(string &s : equations){
+            if(s[1] == '='){
+                unite(s.front(), s.back());
+            }                      
+        }  
+        for(string &s : equations){
+            if(s[1] == '!'){
+                char pa = find(s.front());
+                char pb = find(s.back());
+
+                if(pa == pb) return false;
+            }                      
+        }                 
+        return true;
+    }
+};
