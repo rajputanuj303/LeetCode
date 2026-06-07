d654aa774d70b15ae985a183d6ea1d2d9e6878b7
Anuj Rajput <143288137+rajputanuj303@users.noreply.github.com>
2026-06-07 16:17:15 +0530

    Time: 103 ms (93.30%) | Memory: 259.2 MB (92.02%) - LeetSync

--- 2306-create-binary-tree-from-descriptions/create-binary-tree-from-descriptions.cpp
+++ 2306-create-binary-tree-from-descriptions/create-binary-tree-from-descriptions.cpp
@@ -12,51 +12,41 @@
 class Solution {
 public:
     TreeNode* createBinaryTree(vector<vector<int>>& descriptions) {
+        
         unordered_map<int, TreeNode*> mpp;
-        unordered_set<int> stt;
+        int rootVal = 0;
 
         for(vector<int> &v : descriptions){
             int parentVal = v[0];
             int childVal = v[1];
             bool isLeft = v[2];
 
-            TreeNode* parent = nullptr;
-            TreeNode* child = nullptr;
+            rootVal ^= childVal;
 
-            if(mpp.count(parentVal)){
-                parent = mpp[parentVal];
-            }else{
-                parent = new TreeNode(parentVal);
+            if(!mpp.count(parentVal)){
+                TreeNode* parent = new TreeNode(parentVal);
                 mpp[parentVal] = parent;
+                rootVal ^= parentVal;
             }
 
-            if(mpp.count(childVal)){
-                child = mpp[childVal];
-            }else{
-                child = new TreeNode(childVal);
+            if(!mpp.count(childVal)){
+                TreeNode* child = new TreeNode(childVal);
                 mpp[childVal] = child;
+                rootVal ^= childVal;
             }
-            stt.insert(childVal);
+
+            
 
             if(isLeft){
-                parent->left = child;
+                mpp[parentVal]->left = mpp[childVal];
             }else{
-                parent->right = child;
+                mpp[parentVal]->right = mpp[childVal];
             }
 
-            // cout << parent->val << " " << child->val << endl;
         }
 
-        TreeNode* root = nullptr;
+        // cout << rootVal << endl;
 
-        for(vector<int> &v : descriptions){
-            int parentVal = v[0];
-            if(!stt.count(parentVal)){
-                root = mpp[parentVal];
-                break;
-            }
-        }
-
-        return root;
+        return mpp[rootVal];
     }
 };
