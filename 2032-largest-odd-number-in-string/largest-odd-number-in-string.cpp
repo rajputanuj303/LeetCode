c82089660ca7237535344ca9572008692e045d25
Anuj Rajput <143288137+rajputanuj303@users.noreply.github.com>
2026-06-08 23:19:08 +0530

    Time: 4 ms (23.72%) | Memory: 17.9 MB (17.36%) - LeetSync

--- 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
-    bool iseven(char c){
-        if(c == '2' || c == '4' || c == '6' || c == '8' || c == '0') return true;
-        return false;
-    }
+
     string largestOddNumber(string num) {
         
         int n = num.size();
         int i = n-1;
 
-        while(i >= 0 && iseven(num[i])) i--; 
+        while(i >= 0 && ((num[i]-'0')&1) == 0) i--; 
 
         return num.substr(0, i+1);
     }
