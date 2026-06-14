f4867974b2b636265123f2c54ce663b02126abef
Anuj Rajput <143288137+rajputanuj303@users.noreply.github.com>
2026-06-15 00:09:00 +0530

    Time: 16 ms (53.82%) | Memory: 109.4 MB (96.82%) - LeetSync

--- 2352-design-an-atm-machine/design-an-atm-machine.cpp
+++ 2352-design-an-atm-machine/design-an-atm-machine.cpp
@@ -1,5 +1,6 @@
 class ATM {
 public:
+    vector<int> denoms = {20, 50, 100, 200, 500};
     vector<int> freq;
     ATM() {
         freq.resize(5, 0);
@@ -12,33 +13,22 @@ public:
     }
     
     vector<int> withdraw(int amount) {
+
         vector<int> res(5, 0);
-
-
-        int count500 = min(freq[4], amount/500);
-        amount -= (count500 * 500);
-
-        int count200 = min(freq[3], amount/200);
-        amount -= (count200 * 200);
-
-        int count100 = min(freq[2], amount/100);
-        amount -= (count100 * 100);
-
-        int count50 = min(freq[1], amount/50);
-        amount -= (count50 * 50);
-
-        int count20 = min(freq[0], amount/20);
-        amount -= (count20 * 20);
         
+        for(int i = 4; i>= 0; i--){
+            int cnt = min(freq[i], amount/denoms[i]);
+            amount -= denoms[i]*cnt;
+            res[i] += cnt;
+        }
+
         if(amount) return {-1};
 
-        freq[0] -= count20;
-        freq[1] -= count50;
-        freq[2] -= count100;
-        freq[3] -= count200;
-        freq[4] -= count500;
+        for(int i = 0; i<5; i++){
+            freq[i] -= res[i];
+        }
 
-        return {count20, count50, count100, count200, count500};
+        return res;
     }
 };
 
