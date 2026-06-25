d22e08681690e32e8eb1bb8e961534d8c0ed2cd9
Anuj Rajput <143288137+rajputanuj303@users.noreply.github.com>
2026-06-25 20:32:09 +0530

    Time: 0 ms (100.00%) | Memory: 124.2 MB (36.65%) - LeetSync

--- 2455-node-with-highest-edge-score/node-with-highest-edge-score.cpp
+++ 2455-node-with-highest-edge-score/node-with-highest-edge-score.cpp
@@ -7,13 +7,10 @@ public:
         vector<long long> edgeScore(n, 0);
 
         for(int u = 0; u<n; u++){
-            int v = edges[u];
-
-            edgeScore[v] += u;
+            edgeScore[edges[u]] += u;
         }
 
         int maxIdx = 0;
-
         for(int i = 1; i<n; i++){
             if(edgeScore[i] > edgeScore[maxIdx]) maxIdx = i;
         }
