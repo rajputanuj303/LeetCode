a5ce12e8883a449047eaa9395b1d7e8bcaef1176
Anuj Rajput <143288137+rajputanuj303@users.noreply.github.com>
2026-07-04 01:37:46 +0530

    Time: 51 ms (57.71%) | Memory: 18.9 MB (77.93%) - LeetSync

--- 2206-detonate-the-maximum-bombs/detonate-the-maximum-bombs.cpp
+++ 2206-detonate-the-maximum-bombs/detonate-the-maximum-bombs.cpp
@@ -7,7 +7,7 @@ public:
 
         int res = 1;
 
-        for(int v : adjLs[u]){
+        for(int &v : adjLs[u]){
             if(!visited[v]){
                 res += dfs(v, adjLs, visited);
             }
@@ -21,22 +21,21 @@ public:
 
         vector<vector<int>> adjLs(n);
 
-        for(int i = 0; i<n; i++){
-
-            vector<int> x = bombs[i];
-
-            for(int j = i+1; j<n; j++){
-
-                vector<int> y = bombs[j];
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
 
                 long long dx = 1LL * bombs[j][0] - bombs[i][0];
                 long long dy = 1LL * bombs[j][1] - bombs[i][1];
                 long long distSq = dx * dx + dy * dy;
 
-                if(distSq <= 1LL*x[2]*x[2]) adjLs[i].push_back(j);
-                if(distSq <= 1LL*y[2]*y[2]) adjLs[j].push_back(i);
+                if (distSq <= 1LL * bombs[i][2] * bombs[i][2])
+                    adjLs[i].push_back(j);
+
+                if (distSq <= 1LL * bombs[j][2] * bombs[j][2])
+                    adjLs[j].push_back(i);
             }
         }
+
 
         int res = 0;
         
