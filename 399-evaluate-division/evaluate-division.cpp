38ef6e1821fa4a10caf69b9665919f8b5efc0b91
Anuj Rajput <143288137+rajputanuj303@users.noreply.github.com>
2026-07-08 00:29:30 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (8.79%) - LeetSync

--- 399-evaluate-division/evaluate-division.cpp
+++ 399-evaluate-division/evaluate-division.cpp
@@ -10,9 +10,16 @@ public:
         visited.insert(src);
 
         for(pair<string, double> &p : adjLs[src]){
+
             double ans = dfs(p.first, des, visited);
-            if(ans != -1.0) return p.second * ans;
+            
+            if(ans != -1.0){
+                visited.erase(src);
+                return p.second * ans;
+            }
         }
+
+        visited.erase(src);
 
         return -1.0;
     }
@@ -32,14 +39,14 @@ public:
         int q = queries.size();
 
         vector<double> output(q, -1.0);
+        unordered_set<string> visited;
 
         for(int i = 0; i<q; i++){
             string src = queries[i][0];
-            string des = queries[i][1];
-
-            unordered_set<string> visited;
-            
-            if(adjLs.count(src) && adjLs.count(des)) output[i] = dfs(src, des, visited);
+            string des = queries[i][1];            
+            if(adjLs.count(src) && adjLs.count(des)){
+                output[i] = dfs(src, des, visited);
+            }
         }        
         return output;
     }
