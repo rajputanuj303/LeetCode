053941e24a7a2ad4d32ad34fecfc8f7e83e1a03c
Anuj Rajput <143288137+rajputanuj303@users.noreply.github.com>
2026-07-10 01:14:47 +0530

    Time: 15 ms (62.98%) | Memory: 228.8 MB (47.51%) - LeetSync

--- 3838-path-existence-queries-in-a-graph-i/path-existence-queries-in-a-graph-i.cpp
+++ 3838-path-existence-queries-in-a-graph-i/path-existence-queries-in-a-graph-i.cpp
@@ -1,41 +1,39 @@
 class Solution {
 public:
-    vector<int> parent;
-    int find(int u){
-        if(u == parent[u]) return u;
+    vector<int> parent, sz;
+
+    int find(int u) {
+        if (parent[u] == u) return u;
         return parent[u] = find(parent[u]);
     }
 
-    void Union(int u, int v){
-        
+    void Union(int u, int v) {
         int pu = find(u);
         int pv = find(v);
 
-        if(pu != pv){
-            parent[pv] = pu;
-        }
+        if (pu == pv) return;
+
+        // Attach smaller tree under larger tree
+        if (sz[pu] < sz[pv]) swap(pu, pv);
+
+        parent[pv] = pu;
+        sz[pu] += sz[pv];
     }
+
     vector<bool> pathExistenceQueries(int n, vector<int>& nums, int maxDiff, vector<vector<int>>& queries) {
-        
-        for(int i = 0; i<n; i++) parent.push_back(i);
 
-        for(int i = 1; i<n; i++){
-            if(nums[i]-nums[i-1] <= maxDiff){
-                Union(i, i-1);
-            }
+        parent.resize(n);
+        sz.assign(n, 1);
+
+        for (int i = 0; i < n; i++) parent[i] = i;
+
+        for (int i = 1; i < n; i++) {
+            if (nums[i] - nums[i - 1] <= maxDiff) Union(i, i - 1);
         }
 
-        vector<bool> res;
+        vector<bool> ans;
+        for (auto &q : queries) ans.push_back(find(q[0]) == find(q[1]));
 
-        for(vector<int> &q : queries){
-            
-            int pu = find(q[0]);
-            int pv = find(q[1]);
-
-            if(pu == pv) res.push_back(true);
-            else res.push_back(false);
-        }
-
-        return res;
+        return ans;
     }
 };
