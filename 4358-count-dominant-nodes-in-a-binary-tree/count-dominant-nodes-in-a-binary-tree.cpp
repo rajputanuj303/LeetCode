47911fd037311ec834d042c10488c25909c938ff
Anuj Rajput <143288137+rajputanuj303@users.noreply.github.com>
2026-07-22 00:42:10 +0530

    Time: 50 ms (14.72%) | Memory: 338.4 MB (11.18%) - LeetSync

--- 4358-count-dominant-nodes-in-a-binary-tree/count-dominant-nodes-in-a-binary-tree.cpp
+++ 4358-count-dominant-nodes-in-a-binary-tree/count-dominant-nodes-in-a-binary-tree.cpp
@@ -1,31 +1,21 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    pair<int, int> Solver(TreeNode* root){
+    pair<int, int> dfs(TreeNode* root){
         if(!root) return {0, INT_MIN};
 
-        pair<int, int> left = Solver(root->left);
-        pair<int, int> right = Solver(root->right);
+        auto left = dfs(root->left);
+        auto right = dfs(root->right);
 
         int cMax = max({root->val, left.second, right.second});
         int dCount = left.first + right.first;
 
-        if(root->val == cMax) dCount++;
+        if(root->val == cMax)
+            dCount++;
 
         return {dCount, cMax};
     }
+
     int countDominantNodes(TreeNode* root) {
-        pair<int, int> res = Solver(root);
-        return res.first;
+        return dfs(root).first;
     }
 };
