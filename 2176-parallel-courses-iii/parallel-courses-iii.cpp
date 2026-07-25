a593af9475c36cb48b247e039d6548b01332e8eb
Anuj Rajput <143288137+rajputanuj303@users.noreply.github.com>
2026-07-25 14:49:34 +0530

    Time: 63 ms (47.45%) | Memory: 136.2 MB (69.38%) - LeetSync

--- 2176-parallel-courses-iii/parallel-courses-iii.cpp
+++ 2176-parallel-courses-iii/parallel-courses-iii.cpp
@@ -1,36 +1,39 @@
 class Solution {
 public:
-    vector<int> visited;
-    int dfs(int u, vector<vector<int>> &adjLs, vector<int> &time){
-        
-        if(visited[u] != -1) return visited[u];
-
-        int maxi = 0;
-
-        for(int v : adjLs[u]){
-            maxi = max(maxi, dfs(v, adjLs, time));
-        }
-        return visited[u] = maxi + time[u];
-    }
     int minimumTime(int n, vector<vector<int>>& relations, vector<int>& time) {
-        
-        vector<vector<int>> adjLs(n);
-        vector<int> inDegree(n, 0);
 
-        for(vector<int> &v : relations){
-            adjLs[v[1]-1].push_back(v[0]-1);
-            inDegree[v[0]-1]++;
+        vector<vector<int>> adj(n);
+        vector<int> indegree(n, 0);
+
+        for (auto &e : relations) {
+            adj[e[0] - 1].push_back(e[1] - 1);
+            indegree[e[1] - 1]++;
         }
 
-        int maxi = 0;        
-        visited.resize(n, -1);
+        queue<int> q;
+        vector<int> dist = time;
 
-        for(int i = 0; i<n; i++){
-            if(inDegree[i] == 0){
-                maxi = max(maxi, dfs(i, adjLs, time));
+        for (int i = 0; i < n; i++) {
+            if (indegree[i] == 0)
+                q.push(i);
+        }
+
+        int ans = 0;
+
+        while (!q.empty()) {
+            int u = q.front();
+            q.pop();
+
+            ans = max(ans, dist[u]);
+
+            for (int v : adj[u]) {
+                dist[v] = max(dist[v], dist[u] + time[v]);
+
+                if (--indegree[v] == 0)
+                    q.push(v);
             }
         }
 
-        return maxi;
+        return ans;
     }
 };
