b5833fdf202603c3d2c056ceb2390b5252f48304
Anuj Rajput <143288137+rajputanuj303@users.noreply.github.com>
2026-07-26 01:32:39 +0530

    Time: 27 ms (66.47%) | Memory: 60.3 MB (92.19%) - LeetSync

---
+++ 630-course-schedule-iii/course-schedule-iii.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    static bool cmp(vector<int> &v1, vector<int> &v2){
+        if(v1[1] == v2[1]) return v1[0] < v2[0];
+        return v1[1] < v2[1];
+    }
+    int scheduleCourse(vector<vector<int>>& courses) {
+        
+
+        sort(courses.begin(), courses.end(), cmp);
+
+        priority_queue<int> pq;
+
+        int timeConsumed = 0;
+
+        for(vector<int> &v : courses){
+
+            if(timeConsumed + v[0] <= v[1]){
+                timeConsumed += v[0];
+                pq.push(v[0]);
+            }else if(!pq.empty() && pq.top() > v[0]){
+                timeConsumed -= pq.top();
+                pq.pop();
+                timeConsumed += v[0];
+                pq.push(v[0]);
+            }
+        }
+
+        return pq.size();
+    }
+};
