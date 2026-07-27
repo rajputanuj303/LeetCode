70873f469b4a53e7e7e10bdb44a6e180822b371f
Anuj Rajput <143288137+rajputanuj303@users.noreply.github.com>
2026-07-28 00:13:54 +0530

    Time: 120 ms (28.63%) | Memory: 191.7 MB (24.23%) - LeetSync

---
+++ 3797-design-spreadsheet/design-spreadsheet.cpp
@@ -0,0 +1,57 @@
+class Spreadsheet {
+public:
+    vector<vector<int>> Sheet;
+    Spreadsheet(int rows) {
+        Sheet.resize(rows, vector<int>(26, 0));
+    }
+    
+    void setCell(string cell, int value) {
+        int col = cell[0]-'A';
+        int row = stoi(cell.substr(1)) - 1;
+
+        Sheet[row][col] = value;
+    }
+    
+    void resetCell(string cell) {
+        int col = cell[0]-'A';
+        int row = stoi(cell.substr(1))-1;
+
+        Sheet[row][col] = 0;
+    }
+
+    int SheetFinder(string cell){
+
+        if(cell[0] >= 'A' && cell[0] <= 'Z'){
+            int col = cell[0] - 'A';
+            int row = stoi(cell.substr(1))-1;
+
+            return Sheet[row][col];
+        }
+        return stoi(cell);
+    }
+    
+    int getValue(string formula) {
+        
+        string x, y;
+        int idx = 1;
+
+        while(idx < formula.size() && formula[idx] != '+'){
+            x += formula[idx++];
+        }
+
+        idx++;
+        while(idx < formula.size()){
+            y += formula[idx++];
+        } 
+
+        return SheetFinder(x) + SheetFinder(y);
+    }
+};
+
+/**
+ * Your Spreadsheet object will be instantiated and called as such:
+ * Spreadsheet* obj = new Spreadsheet(rows);
+ * obj->setCell(cell,value);
+ * obj->resetCell(cell);
+ * int param_3 = obj->getValue(formula);
+ */
