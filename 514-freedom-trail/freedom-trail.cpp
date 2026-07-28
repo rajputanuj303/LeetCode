34aedcd46d573d8193c3920db9ccdb319a584d0c
Anuj Rajput <143288137+rajputanuj303@users.noreply.github.com>
2026-07-28 22:31:17 +0530

    Time: 71 ms (22.27%) | Memory: 53 MB (11.90%) - LeetSync

--- 514-freedom-trail/freedom-trail.cpp
+++ 514-freedom-trail/freedom-trail.cpp
@@ -1,36 +1,35 @@
 class Solution {
 public:
     unordered_map<char, vector<int>> mpp;
-    unordered_map<string, int> visited;
+    vector<vector<int>> visited;
 
-    int n;
+    int n, k;
 
     int Solver(int i, int start, string str){
-        if(i >= str.size()) return 0;
 
-        string key = to_string(i) + "#" + to_string(start);
-
-        if(visited.count(key)) return visited[key];
+        if(i >= k) return 0;
+        if(visited[start][i] != -1) return visited[start][i];
 
         char c = str[i];
-
         int minDis = INT_MAX;
 
-        for(int idx : mpp[c]){
-            
+        for(int idx : mpp[c]){            
             int diff = abs(idx - start);
             diff =  min(diff, n - diff);
-
             minDis = min(minDis, 1 + diff + Solver(i+1, idx, str));
         }
-        return visited[key] = minDis;
+
+        return visited[start][i] = minDis;
     }
 
     int findRotateSteps(string ring, string key) {
 
         n = ring.size();
+        k = key.size();
 
-        for(int i = 0; i<ring.size(); i++){
+        visited.assign(n, vector<int>(k, -1));
+
+        for(int i = 0; i<n; i++){
             mpp[ring[i]].push_back(i);
         }
         return Solver(0, 0, key);
