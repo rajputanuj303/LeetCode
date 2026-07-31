384529b8a42ee656d0230e0ca0f2caf13b554af5
Anuj Rajput <143288137+rajputanuj303@users.noreply.github.com>
2026-08-01 01:41:38 +0530

    Time: 35 ms (26.23%) | Memory: 61.8 MB (56.21%) - LeetSync

--- 2355-maximum-consecutive-floors-without-special-floors/maximum-consecutive-floors-without-special-floors.cpp
+++ 2355-maximum-consecutive-floors-without-special-floors/maximum-consecutive-floors-without-special-floors.cpp
@@ -3,14 +3,12 @@ public:
     int maxConsecutive(int bottom, int top, vector<int>& special) {
         
         sort(special.begin(), special.end());
-
-        int i = 0;
+        
         int maxFloors = 0;
 
-        while(i < special.size()){
+        for(int i = 0; i<special.size(); i++){
             maxFloors = max(maxFloors, special[i] - bottom);
             bottom = special[i]+1;
-            i++;
         }
 
         maxFloors = max(maxFloors, top - bottom + 1);
