e6a785b3bb981fecac9087bec5168a290257b04f
Anuj Rajput <143288137+rajputanuj303@users.noreply.github.com>
2026-08-01 16:10:43 +0530

    Time: 167 ms (5.37%) | Memory: 9.6 MB (69.71%) - LeetSync

---
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    pair<int,int> Solver(int i, int j, int chance, vector<int>& nums) {
+        if(i > j) return {0,0};
+
+        if(chance == 1) {
+
+            pair<int, int> left = Solver(i+1, j, 2, nums);
+            pair<int, int> right = Solver(i, j-1, 2, nums);
+
+            // int diffLeft = (nums[i] + left.first) - left.second;
+            // int diffRight = (nums[j] + right.first) - right.second;
+
+            if(nums[i] + left.first >= nums[j] + right.first)
+                return {nums[i] + left.first, left.second};
+            else
+                return {nums[j] + right.first, right.second};
+
+        } else {
+
+            pair<int, int> left = Solver(i+1, j, 1, nums);
+            pair<int, int> right = Solver(i, j-1, 1, nums);
+
+            int diffLeft = (left.second + nums[i]) - left.first;
+            int diffRight = (right.second + nums[j]) - right.first;
+
+            if(diffLeft >= diffRight)
+                return {left.first, nums[i] + left.second};
+            else
+                return {right.first, nums[j] + right.second};
+        }
+    }
+
+    bool predictTheWinner(vector<int>& nums) {
+        int n = nums.size();
+        pair<int, int> res = Solver(0, n-1, 1, nums);
+        return res.first >= res.second;
+    }
+};
