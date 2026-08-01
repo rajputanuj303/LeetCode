67f50dde2a96f19841cac5800f85f262bf944da9
Anuj Rajput <143288137+rajputanuj303@users.noreply.github.com>
2026-08-01 16:15:10 +0530

    Time: 168 ms (5.37%) | Memory: 9.7 MB (69.71%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -1,37 +1,36 @@
 class Solution {
 public:
-    pair<int,int> Solver(int i, int j, int chance, vector<int>& nums) {
-        if(i > j) return {0,0};
+    pair<int, int> Solver(int i, int j, int chance, vector<int> &nums){
+        if(i > j) return {0, 0};
 
-        if(chance == 1) {
+        if(chance == 1){
 
-            pair<int, int> left = Solver(i+1, j, 2, nums);
-            pair<int, int> right = Solver(i, j-1, 2, nums);
+            pair<int, int> pair1 = Solver(i+1, j, 2, nums);
+            pair<int, int> pair2 = Solver(i, j-1, 2, nums);
 
+            if(nums[i] + pair1.first >= nums[j] + pair2.first){
+                return {nums[i] + pair1.first, pair1.second};
+            }else{
+                return {nums[j] + pair2.first, pair2.second};
+            }            
+        }else{
 
-            if(nums[i] + left.first >= nums[j] + right.first)
-                return {nums[i] + left.first, left.second};
-            else
-                return {nums[j] + right.first, right.second};
+            pair<int, int> pair1 = Solver(i+1, j, 1, nums);
+            pair<int, int> pair2 = Solver(i, j-1, 1, nums);
 
-        } else {
-
-            pair<int, int> left = Solver(i+1, j, 1, nums);
-            pair<int, int> right = Solver(i, j-1, 1, nums);
-
-            // int diffLeft = (left.second + nums[i]) - left.first;
-            // int diffRight = (right.second + nums[j]) - right.first;
-
-            if(left.second + nums[i] >= right.second + nums[j])
-                return {left.first, nums[i] + left.second};
-            else
-                return {right.first, nums[j] + right.second};
+            if(nums[i] + pair1.second >= nums[j] + pair2.second){
+                return {pair1.first, nums[i] + pair1.second};
+            }else{
+                return {pair2.first, nums[j] + pair2.second};
+            } 
         }
+
     }
 
     bool predictTheWinner(vector<int>& nums) {
         int n = nums.size();
         pair<int, int> res = Solver(0, n-1, 1, nums);
-        return res.first >= res.second;
+        if(res.first >= res.second) return true;
+        return false;
     }
 };
