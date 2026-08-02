312f5fc28f24d7914f0857b6930770648188632e
Anuj Rajput <143288137+rajputanuj303@users.noreply.github.com>
2026-08-02 08:11:14 +0530

    Time: 19 ms (14.03%) | Memory: 19.6 MB (27.79%) - LeetSync

--- 909-stone-game/stone-game.cpp
+++ 909-stone-game/stone-game.cpp
@@ -1,7 +1,23 @@
 class Solution {
 public:
     
+    vector<vector<int>> dp;
+
+    int Solver(int i, int j, vector<int> &piles){
+        if(i > j) return 0;
+
+        if(dp[i][j] != -1) return dp[i][j];
+
+        int takeLeft  = piles[i] - Solver(i+1, j, piles);
+        int takeRight = piles[j] - Solver(i, j-1, piles);
+
+        return dp[i][j] = max(takeLeft, takeRight);
+    }
+
     bool stoneGame(vector<int>& piles) {
-        return true;
+        int n = piles.size();
+        dp.resize(n+1, vector<int>(n+1, -1));
+        
+        return Solver(0, piles.size()-1, piles) > 0;
     }
 };
