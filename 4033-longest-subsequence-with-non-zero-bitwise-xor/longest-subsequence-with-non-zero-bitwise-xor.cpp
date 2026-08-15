c63ba055782258770fbccaea9285762ffd445ab8
Anuj Rajput <143288137+rajputanuj303@users.noreply.github.com>
2026-08-15 13:29:01 +0530

    Time: 2 ms (45.07%) | Memory: 171.3 MB (56.25%) - LeetSync

--- 4033-longest-subsequence-with-non-zero-bitwise-xor/longest-subsequence-with-non-zero-bitwise-xor.cpp
+++ 4033-longest-subsequence-with-non-zero-bitwise-xor/longest-subsequence-with-non-zero-bitwise-xor.cpp
@@ -4,18 +4,13 @@ public:
         int n = nums.size();
         int totalXor = 0;
 
-        for (int x : nums) {
-            totalXor ^= x;
+        for (int &x : nums) totalXor ^= x;
+
+        if (totalXor != 0) return n;
+
+        for (int &x : nums) {
+            if (x != 0) return n - 1;
         }
-
-        if (totalXor != 0)
-            return n;
-
-        for (int x : nums) {
-            if (x != 0)
-                return n - 1;
-        }
-
         return 0;
     }
 };
