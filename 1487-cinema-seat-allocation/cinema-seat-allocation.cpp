5bef2997898a55cc7c0485730bf9f583548b4700
Anuj Rajput <143288137+rajputanuj303@users.noreply.github.com>
2026-08-20 00:44:05 +0530

    Time: 56 ms (43.58%) | Memory: 66.9 MB (46.96%) - LeetSync

--- 1487-cinema-seat-allocation/cinema-seat-allocation.cpp
+++ 1487-cinema-seat-allocation/cinema-seat-allocation.cpp
@@ -3,10 +3,11 @@ public:
     
     int maxNumberOfFamilies(int n, vector<vector<int>>& reservedSeats) {   
         
-        unordered_map<int, unordered_set<int>> reserved;
+        unordered_map<int, int> reserved;
+
 
         for(vector<int> v : reservedSeats){
-            reserved[v[0]].insert(v[1]);
+            reserved[v[0]] |= (1 << v[1]);
         }
 
         int count = 2 * (n - reserved.size());            
@@ -18,21 +19,21 @@ public:
             bool right = true;
 
             for (int seat = 2; seat <= 5; seat++) {
-                if (seats.count(seat)) {
+                if ((reserved[row] & (1 << seat))) {
                     left = false;
                     break;
                 }
             }
 
             for (int seat = 4; seat <= 7; seat++) {
-                if (seats.count(seat)) {
+                if ((reserved[row] & (1 << seat))) {
                     middle = false;
                     break;
                 }
             }
 
             for (int seat = 6; seat <= 9; seat++) {
-                if (seats.count(seat)) {
+                if ((reserved[row] & (1 << seat))) {
                     right = false;
                     break;
                 }
