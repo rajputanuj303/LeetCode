8a70758ce3f35be22ad03f38bf619c38a4e701be
Anuj Rajput <143288137+rajputanuj303@users.noreply.github.com>
2026-08-20 00:45:21 +0530

    Time: 21 ms (74.83%) | Memory: 60.3 MB (68.07%) - LeetSync

--- 1487-cinema-seat-allocation/cinema-seat-allocation.cpp
+++ 1487-cinema-seat-allocation/cinema-seat-allocation.cpp
@@ -1,47 +1,31 @@
 class Solution {
 public:
-    
-    int maxNumberOfFamilies(int n, vector<vector<int>>& reservedSeats) {   
-        
+    int maxNumberOfFamilies(int n, vector<vector<int>>& reservedSeats) {
+
         unordered_map<int, int> reserved;
 
-
-        for(vector<int> v : reservedSeats){
+        for (auto &v : reservedSeats)
             reserved[v[0]] |= (1 << v[1]);
-        }
 
-        int count = 2 * (n - reserved.size());            
+        int count = 2 * (n - reserved.size());
+
+        // Masks for seats:
+        // Left   : 2,3,4,5
+        // Middle : 4,5,6,7
+        // Right  : 6,7,8,9
+        int left   = (1 << 2) | (1 << 3) | (1 << 4) | (1 << 5);
+        int middle = (1 << 4) | (1 << 5) | (1 << 6) | (1 << 7);
+        int right  = (1 << 6) | (1 << 7) | (1 << 8) | (1 << 9);
 
         for (auto &[row, seats] : reserved) {
 
-            bool left = true;
-            bool middle = true;
-            bool right = true;
+            bool canLeft   = !(seats & left);
+            bool canMiddle = !(seats & middle);
+            bool canRight  = !(seats & right);
 
-            for (int seat = 2; seat <= 5; seat++) {
-                if ((reserved[row] & (1 << seat))) {
-                    left = false;
-                    break;
-                }
-            }
-
-            for (int seat = 4; seat <= 7; seat++) {
-                if ((reserved[row] & (1 << seat))) {
-                    middle = false;
-                    break;
-                }
-            }
-
-            for (int seat = 6; seat <= 9; seat++) {
-                if ((reserved[row] & (1 << seat))) {
-                    right = false;
-                    break;
-                }
-            }
-
-            if (left && right)
+            if (canLeft && canRight)
                 count += 2;
-            else if (left || middle || right)
+            else if (canLeft || canMiddle || canRight)
                 count++;
         }
 
