732fd0ef81d524e8309550fc20c5a62c2d14e3c9
Anuj Singh <rajputanuj303@gmail.com>
2026-09-04 13:50:29 +0530

    Time: 0 ms (100.00%) | Memory: 12.1 MB (66.51%) - LeetSync

--- 133-clone-graph/clone-graph.cpp
+++ 133-clone-graph/clone-graph.cpp
@@ -1,46 +1,19 @@
-/*
-// Definition for a Node.
-class Node {
-public:
-    int val;
-    vector<Node*> neighbors;
-    Node() {
-        val = 0;
-        neighbors = vector<Node*>();
-    }
-    Node(int _val) {
-        val = _val;
-        neighbors = vector<Node*>();
-    }
-    Node(int _val, vector<Node*> _neighbors) {
-        val = _val;
-        neighbors = _neighbors;
-    }
-};
-*/
-
 class Solution {
 public:
     unordered_map<Node*, Node*> visited;
+
     Node* cloneGraph(Node* node) {
+        if (!node) return nullptr;
 
-        if(node == nullptr) return nullptr;
+        if (visited.count(node)) return visited[node];
 
-        if(visited.count(node)) return visited[node];
+        Node* clone = new Node(node->val);
+        visited[node] = clone;
 
-        Node* curr = new Node(node->val, {});
-        visited[node] = curr;
-
-        for(Node* neigh : node->neighbors){
-
-            if(visited.count(neigh)){
-                curr->neighbors.push_back(visited[neigh]);
-                continue;
-            }
-            Node* next = cloneGraph(neigh);
-            curr->neighbors.push_back(next);
+        for (Node* neighbor : node->neighbors) {
+            clone->neighbors.push_back(cloneGraph(neighbor));
         }
 
-        return curr;
+        return clone;
     }
 };
